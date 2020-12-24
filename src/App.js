ee0caf7408f94d99d25a716fe0a00aa55e3453d0
paschalidi <paschalidi@outlook.com>
2020-12-24 12:51:54 +0100

    chore: updates text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          YOU FREAK
+          Having a working version.
         </a>
       </header>
     </div>
