1cade98acf74c2f965aed41d06e5205b10ba39fb
paschalidi <paschalidi@outlook.com>
2021-02-06 16:20:10 +0000

    chore: deploys new version of react app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Having a working version.
+          On Azure.
         </a>
       </header>
     </div>
